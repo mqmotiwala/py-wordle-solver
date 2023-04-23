539de31cd1e77b24050815da21931eedf1aa95e1
mqmotiwala <mqmotiwala@gmail.com>
2023-04-22 21:47:13 -0700

    WIP

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import random
 
 class mufsolver_server(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -17,12 +18,49 @@ class mufsolver_server(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(mufsolver)
     
-    def do_POST(self):
+    def do_POST(self):               
         if self.path == "/guess":
-            guess = {
-                "guess": "rumba",
-                "shout": "why is everybody shouting"}
+            # read game state
+            content_length = int(self.headers['Content-Length'])
+            game_state = self.rfile.read(content_length).decode('utf-8')
+            game_state = json.loads(game_state)
+            print(game_state)
 
+            # prepare guess
+            try: 
+                results = game_state['guess_results']
+                last_guess = results[-1]['guess'] 
+                last_result = results[-1]['result']
+                
+                print(last_guess, last_result)
+                grey_letters = []
+                yellow_letters = []
+                green_letters = []
+                for i, letter in enumerate(last_guess):
+                    if last_result[i] == 0:
+                        grey_letters.append(letter)
+                    elif last_result[i] == 1:
+                        yellow_letters.append((i, letter))
+                    else:
+                        green_letters.append((i, letter))
+                
+                backup_list = words_list
+                for word in words_list:
+                    for letter in green_letters:
+                        if word[letter[0]] != letter[1]:
+                            print(word, word[letter[0]], " is not equal to ", letter[1])
+                            backup_list.remove(word)
+
+                print(backup_list)
+
+            except KeyError:
+                # start of game, no results history
+                print("keyerror")
+
+            guess = {}
+            guess['guess'] = random.choice(words_list)
+
+            # send guess
             guess = json.dumps(guess).encode("utf-8")
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -30,12 +68,12 @@ class mufsolver_server(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(guess)
 
-PORT = 8080
-server = HTTPServer(("", PORT), mufsolver_server)
-server.serve_forever()
-
 words_path = "mufs-solver/words_list.txt"
 words_list = []
 with open(words_path, "r") as f:
     for word in f:
         words_list.append(word.strip())
+
+PORT = 8080
+server = HTTPServer(("", PORT), mufsolver_server)
+server.serve_forever()
