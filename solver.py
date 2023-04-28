8e2bb5458ed4ba249b023223783c521360644870
mqmotiwala <mqmotiwala@gmail.com>
2023-04-27 22:33:28 -0700

    improved speeds, added logic to pick words with max unique letters

--- solver.py
+++ solver.py
@@ -32,14 +32,9 @@ class mufsolver_server(BaseHTTPRequestHandler):
             content_length = int(self.headers['Content-Length'])
             game_state = self.rfile.read(content_length).decode('utf-8')
             game_state = json.loads(game_state)
-
-            # create two lists
-            ## 
-            iterable_list = read_list(filtered_words)
-            words_list = iterable_list.copy()
             
-            # prepare guess
-            try: 
+            # review results
+            if 'guess_results' in game_state:
                 guess_results = game_state['guess_results']
                 last_guess = guess_results[-1]['guess'] 
                 last_result = guess_results[-1]['result']
@@ -55,50 +50,44 @@ class mufsolver_server(BaseHTTPRequestHandler):
                 
                 # check for grey
                 if len(grey_letters):
-                    for word in iterable_list:
+                    for word in words_dict.copy().keys():
                         for letter in word:
                             # need to add extra conditionals for yellow and green letters for words with duplicate letters
                             if letter in grey_letters and letter not in yellow_letters and letter not in green_letters:
-                                words_list.remove(word)
+                                words_dict.pop(word)
                                 # print(word, letter, "is grey")
                                 break
-                    update_list(words_list, filtered_words)
-                    print("Grey check complete. Remaining words:", str(len(words_list)))
+                    print("Grey check complete. Remaining words:", str(len(words_dict)))
 
                 # check for yellow
                 if len(yellow_letters):
-                    iterable_list = read_list(filtered_words)
-                    words_list = iterable_list.copy()
-                    for word in iterable_list:
+                    for word in words_dict.copy().keys():
                         for letter in yellow_letters:
                             # if yellow letter not in word
                             # or, if word has yellow letter in yellow spot
                             if letter not in word or word[yellow_letters[letter]] == letter:
-                                words_list.remove(word)
+                                words_dict.pop(word)
                                 # print(word, letter, "doesn't exist in word or is in pos", str(yellow_letters[letter]))
                                 break
-                    update_list(words_list, filtered_words)
-                    print("Yellow check complete. Remaining words:", str(len(words_list)))
+                    print("Yellow check complete. Remaining words:", str(len(words_dict)))
 
                 # check for green
                 if len(green_letters):
-                    iterable_list = read_list(filtered_words)
-                    words_list = iterable_list.copy()
-                    for word in iterable_list:
+                    for word in words_dict.copy().keys():
                         for letter in green_letters:
                             if letter not in word or word[green_letters[letter]] != letter:
-                                words_list.remove(word)
+                                words_dict.pop(word)
                                 # print(word, letter, "is not in positition", str(green_letters[letter]))
                                 break
-                    update_list(words_list, filtered_words)
-                    print("Green check complete. Remaining words:", str(len(words_list)))
+                    print("Green check complete. Remaining words:", str(len(words_dict)))
 
-                # pick a guess based on latest words_list
-                guess = {'guess':random.choice(words_list)}
-
-            except KeyError:
+            else: 
                 # start of game, no results history
-                guess = {'guess':random.choice(words_list)}
+                pass
+            
+            # pick a guess based on latest words_list
+            guess_word = get_guess(words_dict)
+            guess = {'guess':guess_word}
 
             # send guess
             guess = json.dumps(guess).encode("utf-8")
@@ -113,40 +102,47 @@ class mufsolver_server(BaseHTTPRequestHandler):
             game_results = self.rfile.read(content_length).decode('utf-8')
             game_results = json.loads(game_results)
 
-            game_won = game_results['results']['players'][0]['games_played'][-1]['correct']
+            last_game = game_results['results']['players'][0]['games_played'][-1]
+            if 'correct' in last_game:
+                game_won = last_game['correct'] # should be true
+            else:
+                game_won = False
+
             answer = game_results['results']['games'][0]['answer']
             num_turns = len(game_results['results']['players'][0]['games_played'][-1]['guess_results'])
 
             if game_won:
                 print(f"\nYou correctly guessed {answer} in {num_turns} turns!")
             else:
-                print(f"You lost after {num_turns} turns. The answer was {answer}")
+                print(f"\nYou lost after {num_turns} turns. The answer was {answer}")
             
-            # update filtered list to full list for next game 
-            # # and reset variables 
-            update_list(read_list(all_words), filtered_words)
-            green_letters.clear()
+            # reinitiate words_dict for the game
+            # words_dict = read_dict(words_dict)
+            grey_letters.clear()
             yellow_letters.clear()
             green_letters.clear()
-            
-def read_list(file):
-    list = []
-    with open(file, 'r') as f:
-        for word in f:
-            list.append(word.strip())
+
+def get_guess(words_dict):
+    # build a set of words with the maximum number of unique letters in remaining set
+    max_unique = max(words_dict.values())
+    best_guesses = {k for k,v in words_dict.items() if v == max_unique}
     
-    return list
+    print(f"max_unique = {max_unique}. Remaining words: {len(best_guesses)}")
+    # print(best_guesses)
+    return random.choice(list(best_guesses))
 
-def update_list(list, file):
-    with open(file, 'w') as f:
-        for word in list:
-            f.write(word + '\n')
+def build_dict(words_list_file):
+    with open(words_list_file, 'r') as f:
+        word_dict = {}
+        for word in f:
+            word = word.strip()
+            word_dict[word] = len(set(word))
+        
+    return word_dict
 
-all_words = "words_list.txt"
-filtered_words = 'dynamic_list.txt'
-
-# initiate filtered list as full list prior to game start
-update_list(read_list(all_words), filtered_words)
+# initiate words_dict for the game
+words_list_file = "words_list.txt"
+words_dict = build_dict(words_list_file)
 
 PORT = 8080
 server = HTTPServer(("", PORT), mufsolver_server)
