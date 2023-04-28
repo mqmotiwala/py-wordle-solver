6e9a1e107db1b4d4cffddbc20e49acc8b158b673
mqmotiwala <mqmotiwala@gmail.com>
2023-04-28 14:26:49 -0700

    added logging

--- solver.py
+++ solver.py
@@ -1,6 +1,32 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import random
+import logging
+import datetime as dt
+import os
+
+# logging preferences
+current_time = dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+verbose_format = logging.Formatter("%(asctime)s: %(levelname)s - %(message)s")
+preferred_format = logging.Formatter("%(message)s")
+log_file = f"logs/{current_time}-game.log"
+if not os.path.exists("logs"): os.makedirs("logs")
+
+# file handler
+file_handler = logging.FileHandler(log_file)
+file_handler.setFormatter(preferred_format)
+file_handler.setLevel(logging.INFO) # level=DEBUG will also print the 
+
+# stream handler
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(preferred_format)
+stream_handler.setLevel(logging.WARNING)
+
+# logger settings
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
 
 class mufsolver_server(BaseHTTPRequestHandler):
     def log_message(self, format, *args):
@@ -23,6 +49,7 @@ class mufsolver_server(BaseHTTPRequestHandler):
     
     def do_POST(self):
         # grey letters don't require position information so treat it as a set
+        global words_dict
         grey_letters = set()
         yellow_letters = {}
         green_letters = {}
@@ -32,14 +59,15 @@ class mufsolver_server(BaseHTTPRequestHandler):
             content_length = int(self.headers['Content-Length'])
             game_state = self.rfile.read(content_length).decode('utf-8')
             game_state = json.loads(game_state)
-            
+
             # review results
             if 'guess_results' in game_state:
                 guess_results = game_state['guess_results']
-                last_guess = guess_results[-1]['guess'] 
+                num_guesses = len(guess_results)
+                last_guess = guess_results[-1]['guess']
                 last_result = guess_results[-1]['result']
                 
-                print("\nguess:", last_guess, "result:", last_result)
+                logger.info(f"\nguess #{num_guesses}: {last_guess} - result: {last_result}")
                 for i, letter in enumerate(last_guess):
                     if last_result[i] == 0:
                         grey_letters.add(letter)
@@ -55,9 +83,9 @@ class mufsolver_server(BaseHTTPRequestHandler):
                             # need to add extra conditionals for yellow and green letters for words with duplicate letters
                             if letter in grey_letters and letter not in yellow_letters and letter not in green_letters:
                                 words_dict.pop(word)
-                                # print(word, letter, "is grey")
+                                logger.debug(f"{letter} in {word} is grey")
                                 break
-                    print("Grey check complete. Remaining words:", str(len(words_dict)))
+                    logger.info(f"Grey check complete. Remaining words: {str(len(words_dict))}")
 
                 # check for yellow
                 if len(yellow_letters):
@@ -67,9 +95,9 @@ class mufsolver_server(BaseHTTPRequestHandler):
                             # or, if word has yellow letter in yellow spot
                             if letter not in word or word[yellow_letters[letter]] == letter:
                                 words_dict.pop(word)
-                                # print(word, letter, "doesn't exist in word or is in pos", str(yellow_letters[letter]))
+                                logger.debug(f"{letter} does not exist in {word} or is in position {str(yellow_letters[letter])}")
                                 break
-                    print("Yellow check complete. Remaining words:", str(len(words_dict)))
+                    logger.info(f"Yellow check complete. Remaining words: {str(len(words_dict))}")
 
                 # check for green
                 if len(green_letters):
@@ -77,13 +105,17 @@ class mufsolver_server(BaseHTTPRequestHandler):
                         for letter in green_letters:
                             if letter not in word or word[green_letters[letter]] != letter:
                                 words_dict.pop(word)
-                                # print(word, letter, "is not in positition", str(green_letters[letter]))
+                                logger.debug(f"{letter} is not in position {str(green_letters[letter])}")
                                 break
-                    print("Green check complete. Remaining words:", str(len(words_dict)))
+                    logger.info(f"Green check complete. Remaining words: {str(len(words_dict))}")
 
             else: 
                 # start of game, no results history
-                pass
+                # build words_dict with the full set of words
+                words_dict = build_dict(words_list_file)
+                grey_letters.clear()
+                yellow_letters.clear()
+                green_letters.clear()
             
             # pick a guess based on latest words_list
             guess_word = get_guess(words_dict)
@@ -110,25 +142,20 @@ class mufsolver_server(BaseHTTPRequestHandler):
 
             answer = game_results['results']['games'][0]['answer']
             num_turns = len(game_results['results']['players'][0]['games_played'][-1]['guess_results'])
-
-            if game_won:
-                print(f"\nYou correctly guessed {answer} in {num_turns} turns!")
-            else:
-                print(f"\nYou lost after {num_turns} turns. The answer was {answer}")
             
-            # reinitiate words_dict for the game
-            # words_dict = read_dict(words_dict)
-            grey_letters.clear()
-            yellow_letters.clear()
-            green_letters.clear()
+            logger.info("\n-----------------------------------------\n")
+            if game_won:
+                logger.warning(f"You correctly guessed '{answer}' in {num_turns} turns!")
+            else:
+                logger.warning(f"You lost after {num_turns} turns. The answer was '{answer}'.")
 
 def get_guess(words_dict):
     # build a set of words with the maximum number of unique letters in remaining set
     max_unique = max(words_dict.values())
     best_guesses = {k for k,v in words_dict.items() if v == max_unique}
     
-    print(f"max_unique = {max_unique}. Remaining words: {len(best_guesses)}")
-    # print(best_guesses)
+    logger.info(f"max_unique = {max_unique}. Remaining words: {len(best_guesses)}")
+    logger.info(best_guesses)
     return random.choice(list(best_guesses))
 
 def build_dict(words_list_file):
